dc397384250b00c058cdf1a564421ad53f4e416c
Mahmut Güneş <115086298+Mhmt2534@users.noreply.github.com>
2024-04-22 10:26:53 +0300

    v1.2

---
+++ Migrations/20240421185645_mig2.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPortfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Value",
+                table: "Skills",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ImageHeight",
+                table: "Portfolios",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ImageWidth",
+                table: "Portfolios",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Date",
+                table: "Experiences",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Experiences",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Head",
+                table: "Experiences",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Experiences",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "ToDoLists",
+                columns: table => new
+                {
+                    ToDoListId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoLists", x => x.ToDoListId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoLists");
+
+            migrationBuilder.DropColumn(
+                name: "Value",
+                table: "Skills");
+
+            migrationBuilder.DropColumn(
+                name: "ImageHeight",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "ImageWidth",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Experiences");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Experiences");
+
+            migrationBuilder.DropColumn(
+                name: "Head",
+                table: "Experiences");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Experiences");
+        }
+    }
+}
